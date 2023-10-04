7f786c203ad5be42b7a70e478e09d452955c2bd8
SobolewskiWiktor <sobolsstudio@gmail.com>
2023-10-04 11:12:36 +0200

    testing bike functions

--- Lesson1/src/Main.java
+++ Lesson1/src/Main.java
@@ -4,6 +4,18 @@ public class Main {
         System.out.println("Hello world!");
         bicycle rower = new bicycle(10,5,5);
         System.out.println("C "+rower.cadence+" G "+rower.gear+" S "+rower.speed);
+
+        rower.setGear(6);
+        rower.speedUp(15);
+
+        System.out.println("C "+rower.cadence+" G "+rower.gear+" S "+rower.speed);
+         
+
+        rower.applayBreak(15);
+        rower.setGear(1);
+
+        System.out.println("C "+rower.cadence+" G "+rower.gear+" S "+rower.speed);
+
     }
 
 }
