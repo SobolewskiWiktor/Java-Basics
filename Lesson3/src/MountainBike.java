0b1e8ba261efe143c663cbfe7af7f9b3fdef73eb
SobolewskiWiktor <sobolsstudio@gmail.com>
2023-10-18 10:33:44 +0200

    Lesson 3

---
+++ Lesson3/src/MountainBike.java
@@ -0,0 +1,9 @@
+public class MountainBike  extends Bicycle{
+    public int seatHeight;
+
+    public MountainBike(int startCadence, int startSpeed, int startGear, int startHeight)
+    {
+        super(startCadence, startSpeed, startGear);
+        this.seatHeight = startHeight; 
+    }
+}
